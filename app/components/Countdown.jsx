4b6edcecc7f25a17c788e7a08c252c390bcb2190
philnichols <philnichols@desq.co.uk>
2016-06-17 11:50:37 +0100

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+
+    render: (props) => {
+
+        return (
+            <div>Countdown.jsx</div>
+        )
+    }
+
+});
+
+module.exports = Countdown;
