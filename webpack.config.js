0127e30daeda420ebe41ad3aabf4c780788c1e12
philnichols <philnichols@desq.co.uk>
2016-06-21 14:55:01 +0100

    Support foundation settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var webpack = require('webpack');
 
+var path = require('path');
+
 module.exports = {
     entry: [
         'script!jquery/dist/jquery.min.js',
@@ -43,6 +45,11 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/
             }
+        ]
+    },
+    sassLoader: {
+        includePaths: [
+            path.resolve(__dirname, './node_modules/foundation-sites/scss')
         ]
     },
     devtool: 'cheap-module-eval-source-map'
